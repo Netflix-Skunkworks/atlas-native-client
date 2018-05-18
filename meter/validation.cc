44cf53e9c0776e7c16fe67313022f12f54d9c47d
Daniel Muino <dmuino@users.noreply.github.com>
2018-05-17 18:59:46 -0700

    Add user-friendly function for validation checks (#73)
    Adds a function that can be used by users to get a list of errors or
    warnings that were generated when validation is performed on some Tags
    (including the 'name' key as part of the tags).
    
    This will be used by the node-client when running in a dev setting

--- meter/validation.cc
+++ meter/validation.cc
@@ -7,7 +7,6 @@
 
 namespace atlas {
 namespace meter {
-namespace validation {
 
 static constexpr size_t MAX_KEY_LENGTH = 60;
 static constexpr size_t MAX_VAL_LENGTH = 120;
@@ -22,7 +21,7 @@ static bool is_key_restricted(StrRef k) noexcept {
   return StartsWith(k, "nf.") || StartsWith(k, "atlas.");
 }
 
-const std::unordered_set<StrRef>& valid_nf_tags() {
+static const std::unordered_set<StrRef>& valid_nf_tags() {
   static std::unordered_set<StrRef> kValidNfTags = {
       intern_str("nf.node"),          intern_str("nf.cluster"),
       intern_str("nf.app"),           intern_str("nf.asg"),
@@ -51,13 +50,13 @@ static bool is_user_key_invalid(StrRef k) noexcept {
   return false;
 }
 
-bool empty_or_null(StrRef r) { return r.is_null() || r.length() == 0; }
+static bool empty_or_null(StrRef r) { return r.is_null() || r.length() == 0; }
 
-bool IsValid(const Tags& tags) noexcept {
+bool AreTagsValid(const Tags& tags) noexcept {
   static auto kNameRef = intern_str("name");
 
   std::string err_msg;
-  size_t user_tags = 0;
+  auto user_tags = 0u;
   auto logger = util::Logger();
   auto name_seen = false;
 
@@ -122,6 +121,111 @@ invalid:
   return false;
 }
 
-}  // namespace validation
+ValidationIssues AnalyzeTags(const Tags& tags) noexcept {
+  static auto kNameRef = intern_str("name");
+  auto name_seen = false;
+  auto user_tags = 0u;
+
+  ValidationIssues result;
+  for (const auto& kv : tags) {
+    const auto& k_ref = kv.first;
+    const auto& v_ref = kv.second;
+
+    // check charset
+    auto validated_key = util::ToValidCharset(k_ref);
+    if (validated_key != k_ref) {
+      result.emplace(
+          ValidationIssue::Warn(fmt::format("Key: '{}' will be encoded as '{}'",
+                                            k_ref.get(), validated_key.get())));
+    }
+    auto validated_val = util::EncodeValueForKey(v_ref, k_ref);
+    if (validated_val != v_ref) {
+      result.emplace(ValidationIssue::Warn(
+          fmt::format("Value: '{}' for key '{}' will be encoded as '{}'",
+                      v_ref.get(), k_ref.get(), validated_val.get())));
+    }
+
+    if (empty_or_null(k_ref) || empty_or_null(v_ref)) {
+      result.emplace(
+          ValidationIssue::Err("Tag keys or values cannot be empty"));
+    }
+
+    if (k_ref == kNameRef) {
+      name_seen = true;
+      ++user_tags;
+      auto v_length = v_ref.length();
+      if (v_length > MAX_NAME_LENGTH) {
+        result.emplace(ValidationIssue::Err(
+            fmt::format("value for name exceeds length limit ({} > {})",
+                        v_length, MAX_NAME_LENGTH)));
+      }
+    } else {
+      auto k_length = k_ref.length();
+      auto v_length = v_ref.length();
+      if (k_length > MAX_KEY_LENGTH) {
+        result.emplace(ValidationIssue::Err(
+            fmt::format("Tag key '{}' exceeds length limits ({}-{})",
+                        k_ref.get(), k_length, MAX_KEY_LENGTH)));
+      }
+      if (v_length > MAX_VAL_LENGTH) {
+        result.emplace(ValidationIssue::Err(
+            fmt::format("Tag value '{}' exceeds length limits ({}-{})",
+                        v_ref.get(), v_length, MAX_VAL_LENGTH)));
+      }
+
+      if (!is_key_restricted(k_ref)) {
+        ++user_tags;
+      }
+
+      if (is_user_key_invalid(k_ref)) {
+        result.emplace(ValidationIssue::Err(fmt::format(
+            "Tag key '{}' is using a reserved namespace", k_ref.get())));
+      }
+    }
+  }
+
+  if (user_tags > MAX_USER_TAGS) {
+    result.emplace(
+        ValidationIssue::Err(fmt::format("Too many user tags. There is a limit "
+                                         "of {} user tags. Detected user tags "
+                                         "= {}",
+                                         MAX_USER_TAGS, user_tags)));
+  }
+
+  if (!name_seen) {
+    result.emplace(ValidationIssue::Err("name is a required tag"));
+  }
+  return result;
+}
+
+ValidationIssue ValidationIssue::Err(std::string message) noexcept {
+  ValidationIssue res;
+  res.level = ValidationIssue::Level::ERROR;
+  res.description = std::move(message);
+  return res;
+}
+
+ValidationIssue ValidationIssue::Warn(std::string message) noexcept {
+  ValidationIssue res;
+  res.level = ValidationIssue::Level::WARN;
+  res.description = std::move(message);
+  return res;
+}
+
+bool ValidationIssue::operator<(const ValidationIssue& rhs) const noexcept {
+  if (level < rhs.level) {
+    return true;
+  }
+  if (rhs.level < level) {
+    return false;
+  }
+  return description < rhs.description;
+}
+
+std::string ValidationIssue::ToString() const noexcept {
+  return fmt::format("{}: {}", level == Level::WARN ? "WARN" : "ERROR",
+                     description);
+}
+
 }  // namespace meter
 }  // namespace atlas
--- meter/validation.h
+++ meter/validation.h
@@ -1,15 +1,28 @@
 #pragma once
 
+#include <set>
 #include "id.h"
 
 namespace atlas {
 namespace meter {
-namespace validation {
 
 /// Return whether the given tags will pass validation rules done by our
 /// backends. It assumes we will fix invalid characters
-bool IsValid(const Tags& tags) noexcept;
+bool AreTagsValid(const Tags& tags) noexcept;
 
-}  // namespace validation
+struct ValidationIssue {
+  enum class Level { WARN, ERROR };
+  Level level;
+  std::string description;
+
+  bool operator<(const ValidationIssue& rhs) const noexcept;
+  std::string ToString() const noexcept;
+  static ValidationIssue Err(std::string message) noexcept;
+  static ValidationIssue Warn(std::string message) noexcept;
+};
+
+using ValidationIssues = std::set<ValidationIssue>;
+ValidationIssues AnalyzeTags(const Tags& tags) noexcept;
+
 }  // namespace meter
 }  // namespace atlas
